25eaf6474e63360184cc3537ce8f73bf627b7d70
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-10-23 16:59:25 +0530

    API Service

---
+++ lib/config.dart
@@ -0,0 +1,5 @@
+class Config {
+  static const String appName = "Shopping App";
+  static const String apiURL = '192.168.1.4:4000'; //PRODUCT_URL
+  static const productsAPI = "api/products";
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter_nodejs_crud/model/product_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../config.dart';
+
+class APIService {
+  static var client = http.Client();
+
+  static Future<List<ProductModel>?> getProducts() async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.http(
+      Config.apiURL,
+      Config.productsAPI,
+    );
+
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+
+      return productsFromJson(data["data"]);
+
+      //return true;
+    } else {
+      return null;
+    }
+  }
+
+  static Future<bool> saveProduct(
+    ProductModel model,
+    bool isEditMode,
+    bool isFileSelected,
+  ) async {
+    var productURL = Config.productsAPI;
+
+    if (isEditMode) {
+      // productURL = productURL + "/" + model.productId.toString();
+      productURL = productURL + "/" + model.productId.toString();
+    }
+
+    var url = Uri.http(Config.apiURL, productURL);
+
+    var requestMethod = isEditMode ? "PUT" : "POST";
+
+    var request = http.MultipartRequest(requestMethod, url);
+    request.fields["productName"] = model.productName!;
+    request.fields["productPrice"] = model.productPrice!.toString();
+
+    if (model.productImage != null && isFileSelected) {
+      http.MultipartFile multipartFile = await http.MultipartFile.fromPath(
+        'productImage',
+        model.productImage!,
+      );
+
+      request.files.add(multipartFile);
+    }
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static Future<bool> deleteProduct(productId) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.http(
+      Config.apiURL,
+      Config.productsAPI + "/" + productId,
+    );
+
+    var response = await client.delete(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
