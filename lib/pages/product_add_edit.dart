8fc014513783c9006cb3cfa96bfc1a4f274e334c
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-10-23 16:51:07 +0530

    Product Add_Edit UI Design

--- lib/pages/product_add_edit.dart
+++ lib/pages/product_add_edit.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_nodejs_crud/model/product_model.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+import 'package:snippet_coder_utils/hex_color.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProductAddEdit extends StatefulWidget {
   const ProductAddEdit({super.key});
@@ -8,8 +15,236 @@ class ProductAddEdit extends StatefulWidget {
 }
 
 class _ProductAddEditState extends State<ProductAddEdit> {
+  ProductModel? productModel;
+  static final GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  bool isApiCallProcess = false;
+  List<Object> images = [];
+  bool isEditMode = false;
+  bool isImageSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    productModel = ProductModel();
+
+    Future.delayed(Duration.zero, () {
+      if (ModalRoute.of(context)?.settings.arguments != null) {
+        final Map arguments = ModalRoute.of(context)?.settings.arguments as Map;
+        productModel = arguments['model'];
+        isEditMode = true;
+        setState(() {});
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('NodeJS - CRUD'),
+          elevation: 0,
+        ),
+        backgroundColor: Colors.grey[200],
+        body: ProgressHUD(
+          child: Form(
+            key: globalFormKey,
+            child: productForm(),
+          ),
+          inAsyncCall: isApiCallProcess,
+          opacity: 0.3,
+          key: UniqueKey(),
+        ),
+      ),
+    );
+  }
+
+  Widget productForm() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+              top: 10,
+            ),
+            child: FormHelper.inputFieldWidget(
+              context,
+              // const Icon(Icons.person),
+              "ProductName",
+              "Product Name",
+              (onValidateVal) {
+                if (onValidateVal.isEmpty) {
+                  return 'ProductName can\'t be empty.';
+                }
+
+                return null;
+              },
+              (onSavedVal) => {
+                productModel!.productName = onSavedVal,
+              },
+              initialValue: productModel!.productName ?? "",
+              obscureText: false,
+              borderFocusColor: Colors.black,
+              borderColor: Colors.black,
+              textColor: Colors.black,
+              hintColor: Colors.black.withOpacity(0.7),
+              borderRadius: 10,
+              showPrefixIcon: false,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 10,
+              top: 10,
+            ),
+            child: FormHelper.inputFieldWidget(
+              context,
+              // const Icon(Icons.person),
+              "ProductPrice",
+              "Product Price",
+              (onValidateVal) {
+                if (onValidateVal.isEmpty) {
+                  return 'Product Price can\'t be empty.';
+                }
+
+                return null;
+              },
+              (onSavedVal) => {
+                productModel!.productPrice = int.parse(onSavedVal),
+              },
+              initialValue: productModel!.productPrice == null
+                  ? ""
+                  : productModel!.productPrice.toString(),
+              obscureText: false,
+              borderFocusColor: Colors.black,
+              borderColor: Colors.black,
+              textColor: Colors.black,
+              hintColor: Colors.black.withOpacity(0.7),
+              borderRadius: 10,
+              showPrefixIcon: false,
+              suffixIcon: const Icon(Icons.monetization_on),
+            ),
+          ),
+          picPicker(
+            isImageSelected,
+            productModel!.productImage ?? "",
+            (file) => {
+              setState(
+                () {
+                  productModel!.productImage = file.path;
+                  isImageSelected = true;
+                },
+              )
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: FormHelper.submitButton(
+              "Save",
+              () {
+                if (validateAndSave()) {
+                  // ignore: avoid_print
+                  print(productModel!.toJson());
+                }
+              },
+              btnColor: HexColor("283B71"),
+              borderColor: Colors.white,
+              txtColor: Colors.white,
+              borderRadius: 10,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
+  bool validateAndSave() {
+    final form = globalFormKey.currentState;
+    if (form!.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  static Widget picPicker(
+    bool isImageSelected,
+    String fileName,
+    Function onFilePicked,
+  ) {
+    Future<XFile?> _imageFile;
+    ImagePicker _picker = ImagePicker();
+
+    return Column(
+      children: [
+        fileName.isNotEmpty
+            ? isImageSelected
+                ? Image.file(
+                    File(fileName),
+                    width: 300,
+                    height: 300,
+                  )
+                : SizedBox(
+                    child: Image.network(
+                      fileName,
+                      width: 200,
+                      height: 200,
+                      fit: BoxFit.scaleDown,
+                    ),
+                  )
+            : SizedBox(
+                child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png",
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 35.0,
+              width: 35.0,
+              child: IconButton(
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(Icons.image, size: 35.0),
+                onPressed: () {
+                  _imageFile = _picker.pickImage(source: ImageSource.gallery);
+                  _imageFile.then((file) async {
+                    onFilePicked(file);
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 35.0,
+              width: 35.0,
+              child: IconButton(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                icon: const Icon(Icons.camera, size: 35.0),
+                onPressed: () {
+                  _imageFile = _picker.pickImage(source: ImageSource.camera);
+                  _imageFile.then((file) async {
+                    onFilePicked(file);
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
